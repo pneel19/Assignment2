3316b56df8a6fef41c1f7cb72acbbf39268ba41c
pneel19 <pneel19@gmail.com>
2021-03-03 11:14:20 -0500

    New Changes
    Updated test scenarios.

--- Assignment2Test/TriangleTest.cs
+++ Assignment2Test/TriangleTest.cs
@@ -12,15 +12,48 @@ namespace Assignment2Test
     public class TriangleTest
     {
         [Test]
-        public void testTriangle()
+        public void testTriangleFormation()
         {
             TriangleSolver ts = new TriangleSolver();
 
             string resultArea = ts.Analyze(5, 5, 10);
-            Assert.That(resultArea, Is.EqualTo("It is not a Triangle").IgnoreCase);
+            Assert.AreEqual("It is not a triangle", resultArea);
 
-            string resultArea2 = ts.Analyze(5, 5, 10);
-            Assert.That(resultArea2, Is.EqualTo("It is a triangle and triangle is Isoceles").IgnoreCase);
+            string resultArea2 = ts.Analyze(5, 6, 10);
+            Assert.AreEqual("It is a triangle and the triangle is Scalene", resultArea2);
+        }
+
+        [Test]
+        public void testScalene()
+        {
+            TriangleSolver ts = new TriangleSolver();
+            string resultArea3 = ts.Analyze(6, 5, 10);
+            Assert.AreEqual("It is a triangle and the triangle is Scalene", resultArea3);
+
+            string resultArea4 = ts.Analyze(20, 10, 12);
+            Assert.AreEqual("It is a triangle and the triangle is Scalene", resultArea4);
+        }
+
+        [Test]
+        public void testEquilateral()
+        {
+            TriangleSolver ts = new TriangleSolver();
+            string resultArea5 = ts.Analyze(5, 5, 5);
+            Assert.AreEqual("It is a triangle and the triangle is Equilateral", resultArea5);
+
+            string resultArea6 = ts.Analyze(7, 7, 7);
+            Assert.AreEqual("It is a triangle and the triangle is Equilateral", resultArea6);
+        }
+
+        [Test]
+        public void testIsoceles()
+        {
+            TriangleSolver ts = new TriangleSolver();
+            string resultArea7 = ts.Analyze(6, 6, 10);
+            Assert.AreEqual("It is a triangle and the triangle is Isoceles", resultArea7);
+
+            string resultArea8 = ts.Analyze(15, 8, 8);
+            Assert.AreEqual("It is a triangle and the triangle is Equilateral", resultArea8);
         }
     }
 }
